06830c065c6609bb09c015f0c2bdbe5f3848fef3
Cauã Tavares <tavarescauac@gmail.com>
2023-10-22 12:27:15 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -22,12 +22,49 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.blue,
           title: Text(
-            'Flutter: alooo',
+            'texto',
           ),
         ),
-        body: Container(),
+        body: Container(
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.blue,
+                height: 140,
+              ),
+              Container(
+                color: Colors.white,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      color: Colors.black26,
+                      width: 72,
+                      height: 100,
+                    ),
+                    Text('Aprender Flutter', style: TextStyle(fontSize: 20)),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Icon(Icons.arrow_drop_up),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
         floatingActionButton: FloatingActionButton(onPressed: () {}),
       ),
     );
+  }
+}
+
+class Caixa extends StatelessWidget {
+  const Caixa({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
